d3b1086bff46baa448fc6c38f0daf1bc55a8a82c
qazimoizz <qazimoiz222@gmail.com>
2026-06-22 22:50:32 +0500

    quote-app done

---
+++ script.js
@@ -0,0 +1,12 @@
+const qoute = document.getElementById("quote")
+const author = document.getElementById("author")
+const apiUrl = "https://dummyjson.com/quotes/random"
+async function getData(url) {
+    let response = await fetch(url);
+    let data = await response.json()
+    // console.log(data)
+    qoute.innerText = data.quote
+    author.innerText = data.author
+
+}
+// getData(apiUrl)
